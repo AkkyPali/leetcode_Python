4e89ddd21a068d5dcdf78e4d49137bd2b96185f4
Akanksha Paliwal <akanksha11.aiesec@gmail.com>
2022-02-10 13:20:20 -0800

    .

---
+++ Easy/1_Two_sum.py
@@ -0,0 +1,22 @@
+#using maps 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dictionary = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in dictionary:
+                return i, dictionary[complement]
+            dictionary[nums[i]] = i 
+            print(dictionary)
+
+
+# Brute Force Method 
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i != j:
+                    if nums[i] + nums[j] == target:
+                        return i, j
+        
